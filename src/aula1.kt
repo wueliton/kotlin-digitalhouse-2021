bb2fef7a2c7bd0f8be06b7adde17ce25ba35a436
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-05-05 19:42:00 -0300

    Kotlin - Aula 01

---
+++ src/aula1.kt
@@ -0,0 +1,83 @@
+fun main() {
+    /*println("teste");
+
+    //val - imutável
+    //var - mutável
+    val inteiro: Int = 1
+    val numeroGrande: Long = 832838283283
+    val text: String = "Exemplo"
+    val valor: Double = 10.10
+    val testeVerdade: Boolean = true
+    val decimal: Float = 10.1F
+    var n: Int = 2
+    n++
+
+    //Comparadores & Operadores
+    println(2+2)
+    println(2-2)
+    println(10/2)
+    println(100*20)
+    println(634%2)
+    println(120!=48384)
+    println("Hello" == "hello")
+    println(3/2.0)
+    println(n)
+
+    //Tabela Verdade
+    println(2 == 2)*/
+
+    /*
+    //Listas imutáveis
+    val pets = listOf<String>("cachorro", "gato", "papagaio", "peixe")
+    println(pets.count())
+    println(pets.first())
+    println(pets.last())
+    println(pets[2])
+
+    //Lista mutáveis
+    val petsMutaveis = mutableListOf<String>("cachorro", "gato", "papagaio", "peixe", "coruja")
+    petsMutaveis.add("golfinho")
+
+    println(petsMutaveis)
+     */
+
+    /*
+    val aleatorio: Int = (0..40).random()
+    println(aleatorio)
+    when {
+        aleatorio <= 18.5 -> println("Muito magro")
+        aleatorio <= 24.9 -> println("Peso normal")
+        aleatorio <= 29.9 -> println("Sobrepeso")
+        else -> println("Obesidade")
+    }
+     */
+
+    /*
+    for(num in 1..10 step 4) {
+        println(num)
+    }
+     */
+
+    /*
+    val forList = mutableListOf<String>("cachorro", "gato", "papagaio", "peixe", "coruja")
+
+    for(item in forList) {
+        println(item)
+    }
+
+    forList.forEach { pet ->
+        println(pet)
+    }
+
+    forList.forEach {
+        println(it)
+    }
+     */
+
+    var x: Int = 0
+    while(x < 2) {
+        println(x)
+        x++
+    }
+
+}
